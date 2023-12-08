b6b34707873e033fdacf2060a1e4ec9041e1c443
AndyG181 <andygerald1810895@gmail.com>
2023-12-07 22:15:58 -0600

    TorneoDao sin probar aun

--- DAO/DaoTorneosFinal/src/dao/TorneoDao.java
+++ DAO/DaoTorneosFinal/src/dao/TorneoDao.java
@@ -1,2 +1,36 @@
-package dao;public interface TorneoDao {
+package dao;
+
+import dtos.Torneo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface TorneoDao {
+
+    //obtener todos los torneos
+    List<Torneo> getAllUsers();
+    //crear un torneo
+    boolean createTorneo(Torneo torneo);
+    //modificar info de un torneo
+    boolean modifyTorneo(Torneo torneo);
+    //eliminar torneo
+    boolean deleteTorneo(Torneo torneo);
+    //obtener por id
+    Torneo getById(int id);
+
+
+    /*
+    //obtener por noCuenta
+    Torneo getByNoCuenta(String noCuenta);
+    //obtener los datos personales
+    Torneo getPersonalData(int id);
+    //obtener el id segun el numero de cuenta
+    int getIdByNoCuenta(String noCuenta);
+    LocalDate getFechaNacimiento(int id); */
+
+
+
+
+
+
 }
