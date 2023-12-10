ece8521b4c38607b7c8fab6683d1aebceccee974
ValeriaConmir <valery.itzel.2002@gmail.com>
2023-12-10 01:45:00 -0600

    actualizacion documentacion

--- Capa Negocio/CapaNegocioServlets/src/main/java/com/ids2/capanegocioservlets/RegistroGrupo.java
+++ Capa Negocio/CapaNegocioServlets/src/main/java/com/ids2/capanegocioservlets/RegistroGrupo.java
@@ -13,9 +13,6 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-import db.GrupoDaoImp;
-import dtos.Grupo;
-
 /**
  * Servlet implementation class RegistroGrupo
  */
