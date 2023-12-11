6ffdf722fdbd47d895d2a105777f40df85243ba4
AndyG181 <andygerald1810895@gmail.com>
2023-12-11 01:55:42 -0600

    Servlet torneo

--- Capa Negocio/CapaNegocioServlets/src/main/java/com/ids2/capanegocioservlets/RegistroTorneo.java
+++ Capa Negocio/CapaNegocioServlets/src/main/java/com/ids2/capanegocioservlets/RegistroTorneo.java
@@ -15,6 +15,19 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+/*
+Elaborado por:
+Andy Gerald San Juan Martinez
+
+Valeria Itzel Contreras Miranda
+
+Jose Alejandro Terraza Gonzalez
+
+Brayan Enrique Hernandez Flores
+
+José Daniel Pérez Mejía
+*/
+
 /**
  * Servlet implementation class RegistroTorneo
  */
@@ -42,14 +55,14 @@ public class RegistroTorneo extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         TorneoDaoImp dao;
         Connection con;
-        String idTorneo;
         String nombre;
         String disciplina;
-        int noEquipos;
-        int noGrupos;
-        double porcentajeavance;
+        String noEquipos;
+        String noGrupos;
+        String porcentajeavance;
 
 
         Torneo torneo;
@@ -58,27 +71,41 @@ public class RegistroTorneo extends HttpServlet {
             dao = new TorneoDaoImp();
             dao.setConnection(con);
 
-            id=request.getParameter("idtorneo");
             nombre = request.getParameter("nombre");
             disciplina = request.getParameter("disciplina");
             noEquipos = request.getParameter("noequipos");
             noGrupos = request.getParameter("nogrupos");
             porcentajeavance = request.getParameter("porcentajeavance");
+            fechaInicio = request.getParameter("fechainicio");
+            Date fechaDate = sdf.parse(fechaInicio);
+            Instant instant = fechaDate.toInstant();
+            LocalDate fechaInicio = instant.atZone(ZoneId.systemDefault()).toLocalDate();
+
+            fechaFin = request.getParameter("fechafin");
+            Date fechaDate = sdf.parse(fechaFin);
+            Instant instant = fechaDate.toInstant();
+            LocalDate fechaFin = instant.atZone(ZoneId.systemDefault()).toLocalDate();
+
+
 
 
 
             torneo = new Torneo();
 
-            torneo.setIdTorneo(Integer.parseInt(idTorneo));
             torneo.setNombre(nombre);
-            torneo.set
+            torneo.setDisciplina(disciplina);
+            torneo.setNoEquipos(Integer.parseInt(noEquipos));
+            torneo.setNoGrupos(Integer.parseInt(noGrupos));
+            torneo.setPorcentajeAvance(Double.parseDouble(porcentajeavance));
+            torneo.setFechaInicio(fechaInicio);
+            torneo.setFechaFin(fechaFin);
+
             dao.createTorneo(torneo);
-            response.sendRedirect("altaTorneo.html");
+            response.sendRedirect("torneosregistro.html");
         }catch(Exception e) {
             e.printStackTrace();
         }
 
-        //doGet(request, response);
     }
 
 }
